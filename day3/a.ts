cc25723af1136d9a54d33df51c65271d07c3f585
Siim Lerg <siim.lerg@gmail.com>
2024-12-03 21:50:56 +0200

    feat: add day3 solutions

--- day3/a.ts
+++ day3/a.ts
@@ -1,3 +1,17 @@
+const regex = /mul\(([0-9]{1,3}),([0-9]{1,3})\)/g;
 export const day3A = async (textRows: string[]) => {
-  return 0;
+  const allMatches = textRows
+    .map((row) => [...row.matchAll(regex)])
+    .map((match) => {
+      let sum = 0;
+
+      for (const singleMul of match) {
+        sum += parseInt(singleMul[1], 10) * parseInt(singleMul[2], 10);
+      }
+      return sum;
+    });
+
+  return allMatches.reduce((memo, item) => {
+    return memo + item;
+  }, 0);
 };
--- day3/b.ts
+++ day3/b.ts
@@ -1,3 +1,26 @@
+const regex = /(mul\((\d{1,3}),(\d{1,3})\))|(don't\(\))|(do\(\))/g;
 export const day3B = async (textRows: string[]) => {
-  return 0;
+  let enabled = true;
+  const allMatches = textRows
+    .map((row) => [...row.matchAll(regex)])
+    .map((match) => {
+      let sum = 0;
+
+      for (const singleMul of match) {
+        if (singleMul[0] === "do()") {
+          enabled = true;
+        } else if (singleMul[0] === "don't()") {
+          enabled = false;
+        } else {
+          if (enabled) {
+            sum += parseInt(singleMul[2], 10) * parseInt(singleMul[3], 10);
+          }
+        }
+      }
+      return sum;
+    });
+
+  return allMatches.reduce((memo, item) => {
+    return memo + item;
+  }, 0);
 };
