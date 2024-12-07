f2da0e51731bfbf1cd6fca2f699649408067231e
Siim Lerg <siim.lerg@gmail.com>
2024-12-07 10:33:39 +0200

    feat: adding day7 part 1 and 2

--- day7/a.ts
+++ day7/a.ts
@@ -1,3 +1,46 @@
+import { toInt } from "../lib/string.ts";
+
 export const day7A = (textRows: string[]) => {
-  return 0;
+  const data = textRows
+    .map((row) => row.split(": "))
+    .map(([total, parts]) => ({
+      total: toInt(total),
+      parts: parts.split(" ").map(toInt),
+    }));
+
+  let total = 0;
+  for (const equation of data) {
+    if (isValidEquation(equation.total, equation.parts)) {
+      total += equation.total;
+    }
+  }
+  return total;
 };
+
+function isValidEquation(
+  expectedTotal: number,
+  parts: number[],
+  accumulatedTotal: number = 0,
+) {
+  if (accumulatedTotal === expectedTotal) {
+    return true;
+  }
+
+  if (parts.length === 0) {
+    return false;
+  }
+
+  if (
+    isValidEquation(expectedTotal, parts.slice(1), accumulatedTotal + parts[0])
+  ) {
+    return true;
+  }
+
+  if (
+    isValidEquation(expectedTotal, parts.slice(1), accumulatedTotal * parts[0])
+  ) {
+    return true;
+  }
+
+  return false;
+}
--- day7/b.ts
+++ day7/b.ts
@@ -1,3 +1,62 @@
+import { toInt } from "../lib/string.ts";
+
 export const day7B = (textRows: string[]) => {
-  return 0;
+  const data = textRows
+    .map((row) => row.split(": "))
+    .map(([total, parts]) => ({
+      total: toInt(total),
+      parts: parts.split(" ").map(toInt),
+    }));
+
+  let total = 0;
+  for (const equation of data) {
+    if (isValidEquation(equation.total, equation.parts)) {
+      total += equation.total;
+    }
+  }
+  return total;
 };
+
+function isValidEquation(
+  expectedTotal: number,
+  parts: number[],
+  accumulatedTotal: number = 0,
+) {
+  if (accumulatedTotal === expectedTotal && parts.length === 0) {
+    return true;
+  }
+
+  if (parts.length === 0) {
+    return false;
+  }
+
+  if (
+    isValidEquation(expectedTotal, parts.slice(1), accumulatedTotal + parts[0])
+  ) {
+    return true;
+  }
+
+  if (accumulatedTotal > 0) {
+    if (
+      isValidEquation(
+        expectedTotal,
+        parts.slice(1),
+        accumulatedTotal * parts[0],
+      )
+    ) {
+      return true;
+    }
+
+    if (
+      isValidEquation(
+        expectedTotal,
+        parts.slice(1),
+        toInt(`${accumulatedTotal}${parts[0]}`),
+      )
+    ) {
+      return true;
+    }
+  }
+
+  return false;
+}
