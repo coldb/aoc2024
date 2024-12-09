f424a00969adfbbdd0a409a6d4d3c380d6284335
Siim Lerg <siim.lerg@gmail.com>
2024-12-09 22:09:40 +0200

    feat: adding day 9 step 1 and 2

--- day9/a.ts
+++ day9/a.ts
@@ -1,3 +1,47 @@
+import { toInt } from "../lib/string.ts";
+
 export const day9A = (textRows: string[]) => {
-  return 0;
+  const diskMap = textRows[0].split("").map(toInt);
+
+  const spacesIdx: number[] = [];
+  const data: { idx: number; value: number }[] = [];
+  let currentIdx = 0;
+  let currentValue = 0;
+
+  for (let i = 0; i < diskMap.length; i++) {
+    const value = diskMap[i];
+
+    // file
+    if (i % 2 === 0) {
+      for (let j = 0; j < value; j++) {
+        data.push({ idx: currentIdx, value: currentValue });
+        currentIdx++;
+      }
+
+      currentValue++;
+    }
+    // space
+    else {
+      for (let j = 0; j < value; j++) {
+        spacesIdx.push(currentIdx);
+        currentIdx++;
+      }
+    }
+  }
+
+  for (let i = 0; i < spacesIdx.length; i++) {
+    const lastData = data[data.length - 1 - i];
+    const nextSpaceIdx = spacesIdx[i];
+
+    if (nextSpaceIdx > lastData.idx) {
+      break;
+    }
+    lastData.idx = spacesIdx[i];
+  }
+
+  const checksum = data.reduce((memo, item) => {
+    return memo + item.idx * item.value;
+  }, 0);
+
+  return checksum;
 };
--- day9/b.ts
+++ day9/b.ts
@@ -1,3 +1,73 @@
+import { toInt } from "../lib/string.ts";
+
 export const day9B = (textRows: string[]) => {
-  return 0;
+  const diskMap = textRows[0].split("").map(toInt);
+
+  const spaces: { startIdx: number; length: number }[] = [];
+  const data: { idx: number; value: number }[] = [];
+  const dataParts: {
+    length: number;
+    values: { idx: number; value: number }[];
+  }[] = [];
+
+  let currentIdx = 0;
+  let currentValue = 0;
+
+  for (let i = 0; i < diskMap.length; i++) {
+    const value = diskMap[i];
+
+    // file
+    if (i % 2 === 0) {
+      if (value > 0) {
+        const dataPart: dataParts["values"] = { length: value, values: [] };
+        for (let j = 0; j < value; j++) {
+          const singleData = { idx: currentIdx, value: currentValue };
+
+          data.push(singleData);
+          dataPart.values.push(singleData);
+          currentIdx++;
+        }
+        dataParts.push(dataPart);
+      }
+
+      currentValue++;
+    }
+    // space
+    else {
+      if (value === 0) {
+        continue;
+      }
+
+      spaces.push({ startIdx: currentIdx, length: value });
+      currentIdx += value;
+    }
+  }
+
+  for (let i = 0; i < dataParts.length; i++) {
+    const currentDataPart = dataParts[dataParts.length - 1 - i];
+
+    for (let j = 0; j < spaces.length; j++) {
+      const space = spaces[j];
+
+      if (currentDataPart.values[0].idx < space.startIdx) {
+        break;
+      }
+
+      if (space.length >= currentDataPart.length) {
+        for (let k = 0; k < currentDataPart.length; k++) {
+          currentDataPart.values[k].idx = space.startIdx + k;
+        }
+
+        space.length -= currentDataPart.length;
+        space.startIdx += currentDataPart.length;
+        break;
+      }
+    }
+  }
+
+  const checksum = data.reduce((memo, item) => {
+    return memo + item.idx * item.value;
+  }, 0);
+
+  return checksum;
 };
