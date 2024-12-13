07aad236f59756dd2b11e99701af29ed29e76ca7
Siim Lerg <siim.lerg@gmail.com>
2024-12-13 17:30:23 +0200

    feat: day 13 step 2

--- day13/a.ts
+++ day13/a.ts
@@ -4,38 +4,76 @@ import type { DaySolution } from "../types.ts";
 export const dayPart1 = (textRows: string[]) => {
   const data = splitData(textRows);
 
-  const finalData: number | undefined = data
-    .map((game) => {
-      const buttonAParts = game[0].split(" ");
-      const buttonBParts = game[1].split(" ");
-      const goalParts = game[2].split(" ");
+  const finalData = data.map((game) => {
+    const buttonAParts = game[0].split(" ");
+    const buttonBParts = game[1].split(" ");
+    const goalParts = game[2].split(" ");
 
-      const bestCost = { best: Number.MAX_VALUE };
-      solve(
-        {
-          buttonA: {
-            x: toInt(buttonAParts[2].substring(2, buttonAParts[2].length - 1)),
-            y: toInt(buttonAParts[3].substring(2)),
-          },
-          buttonB: {
-            x: toInt(buttonBParts[2].substring(2, buttonBParts[2].length - 1)),
-            y: toInt(buttonBParts[3].substring(2)),
-          },
-          goal: {
-            x: toInt(goalParts[1].substring(2, goalParts[1].length - 1)),
-            y: toInt(goalParts[2].substring(2)),
-          },
-        },
-        bestCost,
-        new Set<string>(),
+    const gameData: Game = {
+      buttonA: {
+        x: toInt(buttonAParts[2].substring(2, buttonAParts[2].length - 1)),
+        y: toInt(buttonAParts[3].substring(2)),
+      },
+      buttonB: {
+        x: toInt(buttonBParts[2].substring(2, buttonBParts[2].length - 1)),
+        y: toInt(buttonBParts[3].substring(2)),
+      },
+      goal: {
+        x: toInt(goalParts[1].substring(2, goalParts[1].length - 1)),
+        y: toInt(goalParts[2].substring(2)),
+      },
+    };
+
+    const buttonAMax = Math.min(
+      Math.floor(gameData.goal.x / gameData.buttonA.x),
+      Math.floor(gameData.goal.y / gameData.buttonA.y),
+    );
+
+    let lastButtonBAmount = 0;
+    let bestPrice = Number.MAX_VALUE;
+    for (let i = buttonAMax; i > 0; i--) {
+      const surplussX = gameData.goal.x - i * gameData.buttonA.x;
+      const surplussY = gameData.goal.y - i * gameData.buttonA.y;
+
+      const buttonBAmount = Math.min(
+        Math.floor(surplussX / gameData.buttonB.x),
+        Math.floor(surplussY / gameData.buttonB.y),
       );
 
-      return bestCost.best;
-    })
+      if (buttonBAmount === lastButtonBAmount) {
+        continue;
+      }
+      lastButtonBAmount = buttonBAmount;
+
+      const currentPrice = i * 3 + buttonBAmount;
+
+      if (currentPrice > bestPrice) {
+        break;
+      }
+
+      const isXDivisible = surplussX % gameData.buttonB.x === 0;
+      const isYDivisible = surplussY % gameData.buttonB.y === 0;
+
+      const xTotal =
+        i * gameData.buttonA.x + buttonBAmount * gameData.buttonB.x;
+      const yTotal =
+        i * gameData.buttonA.y + buttonBAmount * gameData.buttonB.y;
+      if (
+        isXDivisible &&
+        isYDivisible &&
+        xTotal === gameData.goal.x &&
+        yTotal === gameData.goal.y
+      ) {
+        bestPrice = Math.min(currentPrice, bestPrice);
+      }
+    }
+
+    return bestPrice;
+  });
+
+  return finalData
     .filter((value) => value !== Number.MAX_VALUE)
     .reduce((memo, item) => memo + item, 0);
-
-  return finalData;
 };
 
 type Game = {
@@ -43,44 +81,6 @@ type Game = {
   buttonB: { x: number; y: number };
   goal: { x: number; y: number };
 };
-
-function solve(
-  game: Game,
-  bestCost: { best: number },
-  calculated: Set<string>,
-  aPressed: number = 0,
-  bPressed: number = 0,
-) {
-  const currentCost = aPressed * 3 + bPressed;
-
-  if (calculated.has(`${aPressed}-${bPressed}`)) {
-    return true;
-  }
-
-  calculated.add(`${aPressed}-${bPressed}`);
-
-  if (currentCost > bestCost.best) {
-    // return Number.MAX_VALUE;
-    return true;
-  }
-
-  const xSum = aPressed * game.buttonA.x + bPressed * game.buttonB.x;
-  const ySum = aPressed * game.buttonA.y + bPressed * game.buttonB.y;
-
-  if (game.goal.x === xSum && game.goal.y === ySum) {
-    bestCost.best = Math.min(currentCost, bestCost.best);
-    // return aPressed * 3 + bPressed;
-    return true;
-  }
-
-  if (xSum >= game.goal.x || ySum >= game.goal.y) {
-    // return Number.MAX_VALUE;
-    return true;
-  }
-
-  solve(game, bestCost, calculated, aPressed + 1, bPressed);
-  solve(game, bestCost, calculated, aPressed, bPressed + 1);
-}
 
 export const solution: DaySolution = {
   fn: dayPart1,
