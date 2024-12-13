f63e48de2a90e9c899a286761056d4dec4cc381c
Siim Lerg <siim.lerg@gmail.com>
2024-12-13 21:21:35 +0200

    feat: adding day 14 placeholder

--- day14/b.ts
+++ day14/b.ts
@@ -1,11 +1,11 @@
 import type { DaySolution } from "../types.ts";
 
-export const dayPart1 = (textRows: string[]) => {
+export const dayPart2 = (textRows: string[]) => {
   return 0;
 };
 
 export const solution: DaySolution = {
-  fn: dayPart1,
+  fn: dayPart2,
   expectedSample: -1,
   expected: -1,
 };
