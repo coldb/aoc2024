01613cf889a873f264db1166f1134cab559a4282
Siim Lerg <siim.lerg@gmail.com>
2024-12-20 00:59:16 +0200

    feat: adding day 19 part 1 and 2

--- day19/a.ts
+++ day19/a.ts
@@ -1,11 +1,42 @@
+import { splitData } from "../lib/string.ts";
 import type { DaySolution } from "../types.ts";
 
 export const dayPart1 = (textRows: string[]) => {
-  return 0;
+  const [towelsStr, patterns] = splitData(textRows);
+
+  const towels = towelsStr[0].split(", ");
+
+  let count = 0;
+  for (const pattern of patterns) {
+    if (isPatternPossible(pattern, towels)) {
+      count++;
+    }
+  }
+  return count;
 };
+
+function isPatternPossible(
+  pattern: string,
+  towels: string[],
+  pointer: number = 0,
+) {
+  if (pointer === pattern.length) {
+    return true;
+  }
+
+  for (const towel of towels) {
+    if (pattern.substring(pointer).startsWith(towel)) {
+      if (isPatternPossible(pattern, towels, pointer + towel.length)) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
 
 export const solution: DaySolution = {
   fn: dayPart1,
-  expectedSample: -1,
-  expected: -1,
+  expectedSample: 6,
+  expected: 263,
 };
--- day19/b.ts
+++ day19/b.ts
@@ -1,11 +1,49 @@
+import { splitData, toInt } from "../lib/string.ts";
 import type { DaySolution } from "../types.ts";
 
 export const dayPart2 = (textRows: string[]) => {
-  return 0;
+  const [towelsStr, patterns] = splitData(textRows);
+
+  const towels = towelsStr[0].split(", ");
+
+  let count = 0;
+  for (const pattern of patterns) {
+    count += findCharMatches(pattern, towels);
+  }
+
+  return count;
 };
+
+function findCharMatches(pattern: string, towels: string[]) {
+  const permutationsPerLength = new Map<number, number>();
+
+  for (let i = 0; i < pattern.length; i++) {
+    const remainingPattern = pattern.substring(i);
+    for (const towel of towels) {
+      if (remainingPattern.startsWith(towel)) {
+        if (i === 0) {
+          permutationsPerLength.set(
+            towel.length,
+            (permutationsPerLength.get(towel.length) ?? 0) + 1,
+          );
+        } else {
+          const newLength = i + towel.length;
+
+          permutationsPerLength.set(
+            newLength,
+            (permutationsPerLength.get(i) ?? 0) +
+              (permutationsPerLength.get(newLength) ?? 0),
+          );
+        }
+      }
+    }
+  }
+
+  return permutationsPerLength.get(pattern.length) ?? 0;
+}
 
 export const solution: DaySolution = {
   fn: dayPart2,
-  expectedSample: -1,
-  expected: -1,
+  expectedSample: 16,
+  expected: 723524534506343,
 };
