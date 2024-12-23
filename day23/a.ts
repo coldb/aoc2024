9cbecceb85a131e4ba5f5d889b7a897ecdfdb40d
Siim Lerg <siim.lerg@gmail.com>
2024-12-23 08:26:32 +0200

    feat: adding day 23 part 1 and part 2

--- day23/a.ts
+++ day23/a.ts
@@ -1,11 +1,77 @@
 import type { DaySolution } from "../types.ts";
 
+type Computer = {
+  name: string;
+  connectedTo: Set<string>;
+};
+
 export const dayPart1 = (textRows: string[]) => {
-  return 0;
+  const connections = textRows.map((row) => row.split("-"));
+  const setsOfThree = new Set<string>();
+
+  const computers = new Map<string, Computer>();
+
+  for (const connection of connections) {
+    const [firstName, secondName] = connection;
+
+    const firstComputer = computers.get(firstName);
+
+    if (firstComputer !== undefined) {
+      firstComputer.connectedTo.add(secondName);
+    } else {
+      computers.set(firstName, {
+        name: firstName,
+        connectedTo: new Set([secondName]),
+      });
+    }
+
+    const secondComputer = computers.get(secondName);
+
+    if (secondComputer !== undefined) {
+      secondComputer.connectedTo.add(firstName);
+    } else {
+      computers.set(secondName, {
+        name: secondName,
+        connectedTo: new Set([firstName]),
+      });
+    }
+  }
+
+  for (const computer of computers.values()) {
+    const computerName = computer.name;
+    const connectedTo = Array.from(computer.connectedTo);
+
+    for (let i = 0; i < connectedTo.length; i++) {
+      for (let j = 0; j < connectedTo.length; j++) {
+        if (i === j) {
+          continue;
+        }
+        const firstRelationName = connectedTo[i];
+        const secondRelationName = connectedTo[j];
+
+        if (
+          computers
+            .get(firstRelationName)
+            ?.connectedTo.has(secondRelationName) &&
+          [computerName, firstRelationName, secondRelationName].some((name) =>
+            name.startsWith("t"),
+          )
+        ) {
+          setsOfThree.add(
+            [computerName, firstRelationName, secondRelationName]
+              .toSorted()
+              .join(","),
+          );
+        }
+      }
+    }
+  }
+
+  return setsOfThree.size;
 };
 
 export const solution: DaySolution = {
   fn: dayPart1,
-  expectedSample: -1,
-  expected: -1,
+  expectedSample: 7,
+  expected: 1368,
 };
--- day23/b.ts
+++ day23/b.ts
@@ -1,11 +1,77 @@
+import {
+  permutations as permutationsFn,
+  powerSet,
+} from "https://deno.land/x/combinatorics/mod.ts";
 import type { DaySolution } from "../types.ts";
 
-export const dayPart2 = (textRows: string[]) => {
-  return 0;
+type Computer = {
+  name: string;
+  connectedTo: Set<string>;
 };
 
+export const dayPart2 = (textRows: string[]) => {
+  const connections = textRows.map((row) => row.split("-"));
+
+  const setsOfTwo = new Set<string>();
+  const computers = new Map<string, Computer>();
+
+  for (const connection of connections) {
+    const [firstName, secondName] = connection;
+
+    setsOfTwo.add(`${firstName}-${secondName}`);
+    setsOfTwo.add(`${secondName}-${firstName}`);
+
+    const firstComputer = computers.get(firstName);
+
+    if (firstComputer !== undefined) {
+      firstComputer.connectedTo.add(secondName);
+    } else {
+      computers.set(firstName, {
+        name: firstName,
+        connectedTo: new Set([secondName]),
+      });
+    }
+
+    const secondComputer = computers.get(secondName);
+
+    if (secondComputer !== undefined) {
+      secondComputer.connectedTo.add(firstName);
+    } else {
+      computers.set(secondName, {
+        name: secondName,
+        connectedTo: new Set([firstName]),
+      });
+    }
+  }
+
+  let biggestLan = 0;
+  let biggestLanComputers: string[] = [];
+
+  for (const computer of computers.values()) {
+    const connectedTo = Array.from(computer.connectedTo);
+    connectedTo.push(computer.name);
+    const permutations = powerSet(connectedTo);
+
+    for (const permutation of permutations) {
+      if (permutation.length > biggestLan) {
+        const allCombinations = permutationsFn(permutation, 2);
+
+        if (
+          allCombinations.every(([first, second]) =>
+            setsOfTwo.has(`${first}-${second}`),
+          )
+        ) {
+          biggestLan = permutation.length;
+          biggestLanComputers = permutation;
+        }
+      }
+    }
+  }
+
+  return biggestLanComputers.toSorted().join(",");
+};
 export const solution: DaySolution = {
   fn: dayPart2,
-  expectedSample: -1,
-  expected: -1,
+  expectedSample: "co,de,ka,ta",
+  expected: "dd,ig,il,im,kb,kr,pe,ti,tv,vr,we,xu,zi",
 };
