953640cb8a2535c39108006e110220ddff8b79c7
Siim Lerg <siim.lerg@gmail.com>
2024-12-25 00:11:07 +0200

    feat: adding day 24 part 1

--- day24/a.ts
+++ day24/a.ts
@@ -1,11 +1,88 @@
+import { splitData, toInt } from "../lib/string.ts";
 import type { DaySolution } from "../types.ts";
 
+type Operartion = {
+  i1: string;
+  i2: string;
+  outputKey: string;
+  operation: "AND" | "XOR" | "OR";
+};
 export const dayPart1 = (textRows: string[]) => {
-  return 0;
+  const [initialValues, operationsStr] = splitData(textRows);
+
+  const values = new Map<string, number>();
+  const operations: {
+    i1: string;
+    i2: string;
+    outputKey: string;
+    operation: "AND" | "XOR" | "OR";
+  }[] = operationsStr.map((operationStr) => {
+    const [, i1, operation, i2, outputKey] =
+      /^(.+?) (.+?) (.+?) -> (.+?)$/.exec(operationStr);
+
+    return { i1, i2, outputKey, operation };
+  });
+
+  initialValues.forEach((valueStr) => {
+    const [key, value] = valueStr.split(": ");
+    values.set(key, toInt(value));
+  });
+
+  let count = 0;
+
+  do {
+    for (let i = operations.length - 1; i >= 0; i--) {
+      const operation = operations[i];
+
+      if (values.has(operation.i1) && values.has(operation.i2)) {
+        operations.splice(i, 1);
+
+        const i1Value = values.get(operation.i1);
+        const i2Value = values.get(operation.i2);
+        if (operation.operation === "AND") {
+          values.set(operation.outputKey, i1Value === 1 && i1Value === i2Value ? 1 : 0);
+        } else if (operation.operation === "OR") {
+          values.set(
+            operation.outputKey,
+            i1Value === 0 && i2Value === 0 ? 0 : 1,
+          );
+        } else if (operation.operation === "XOR") {
+          values.set(operation.outputKey, i1Value !== i2Value ? 1 : 0);
+        }
+      }
+      // console.log(operation);
+    }
+
+    // console.log(operations, operations.length);
+    // count++;
+    // if (count > 3) {
+    //   break;
+    // }
+  } while (operations.length > 0);
+  console.log(Array.from(values.entries())
+        .filter(([key]) => key.startsWith("z")).sort((a, b) => a[0].localeCompare(b[0])))
+
+      console.log(Array.from(values.entries())
+        .filter(([key]) => key.startsWith("z"))
+        .sort((a, b) => a[0].localeCompare(b[0]))
+        .map(([, v]) => v)
+        .reverse()
+        .join(""))
+
+  return (
+    parseInt(
+      Array.from(values.entries())
+        .filter(([key]) => key.startsWith("z"))
+        .sort((a, b) => a[0].localeCompare(b[0]))
+        .map(([, v]) => v)
+        .reverse()
+        .join(""),
+       2),
+  );
 };
 
 export const solution: DaySolution = {
   fn: dayPart1,
-  expectedSample: -1,
-  expected: -1,
+  expectedSample: 2024,
+  expected: 53325321422566, // 54699814766890 to high
 };
